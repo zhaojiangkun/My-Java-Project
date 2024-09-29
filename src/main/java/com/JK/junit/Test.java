2a28f7dbd1ae8952e5c3c25ba55c862b17885c72
Jinit <jiangkun_zhao@163.com>
2024-09-29 20:52:26 +0800

    init

---
+++ src/main/java/com/JK/junit/Test.java
@@ -0,0 +1,12 @@
+package com.JK.junit;
+
+/**
+ * @Author: ZhaoJiangKun
+ * @Date: 2024-09-29 20:51
+ */
+public class Test {
+
+    public void test(){
+        System.out.println("helloWorld");
+    }
+}
